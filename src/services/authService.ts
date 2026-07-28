7198d67fdaeac838e2af87c93090c08b0c5e5923
khalid altaj <khalid.altaj.94@gmail.com>
2026-07-28 19:14:59 +0400

    إزالة الـ analytics لتجنب أخطاء الشبكة على غيت هاب بايجز

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -19,7 +18,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-export const db = getFirestore(app); // 2. أضف هذا السطر لتصدير قاعدة البيانات
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
 
 export { auth };
